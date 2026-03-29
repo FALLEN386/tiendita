92d2135036aeeb1666239730df432d35609ae4ea
thefallen <contacto@thefallen.cl>
2026-03-28 20:21:23 -0600

    desafio desaparecer footer

--- final.js
+++ final.js
@@ -1,10 +1,29 @@
 
   const carrito = document.getElementById("carrito");
   const template = document.getElementById("template");
+  const footer = document.getElementById('footer');
+  const templateFooter = document.getElementById('templateFooter');
    const fragment = document.createDocumentFragment();
-  const botones = document.querySelectorAll('.card .btn');
+//   const botones = document.querySelectorAll('.card .btn');
 
-const carritoObjeto = [];
+
+document.addEventListener('click', e => {
+    //console.log(e.target.matches(".card .btn-outline-secondary"))
+    if (e.target.matches(".card .btn-outline-secondary")) {
+        //console.log('ejecutar agregar al carro')
+        agregarAlCarrito(e)
+    }
+
+    //console.log(e.target.matches(".list-group-item .btn-success"));
+    if (e.target.matches("#carrito .list-group-item .btn-success")) {
+        btnAumentar(e)
+    }
+    if (e.target.matches("#carrito .list-group-item .btn-danger")) {
+        btnDisminuir(e)
+    }
+});
+
+let carritoObjeto = [];
 
  //1
   const agregarAlCarrito = (e) => {
@@ -14,43 +33,99 @@ const carritoObjeto = [];
           titulo: e.target.dataset.fruta,
           id: e.target.dataset.fruta,
           cantidad: 1,
+          precio: parseInt( e.target.dataset.precio),
       };
+
+      //console.log(producto);
           //1
-      const indice = carritoObjeto.findIndex(
+       const indice = carritoObjeto.findIndex(
           (item) => item.id === producto.id      )
 
-      console.log(indice);
+      //console.log(indice);
 
       //2
-      if (indice === -1) {
+       if (indice === -1) {
           carritoObjeto.push(producto)
       } else { //3
-         carritoObjeto[indice].cantidad ++
+           carritoObjeto[indice].cantidad++
+        //    carritoObjeto[indice].precio = carritoObjeto[indice].cantidad * producto.precio 
       }
-        //4
+        
+
       console.log(carritoObjeto);
 
 
-     pintarCarrito(carritoObjeto);
+     pintarCarrito();
   };
 
-  const pintarCarrito = (array) => {
-     carrito.textContent = ""
+const pintarCarrito = (array) => {
+    carrito.textContent = ""
     
-     array.forEach((item) => {
-         const clone = template.content.firstElementChild.cloneNode(true);
-         clone.querySelector('.lead').textContent = item.titulo
+    carritoObjeto.forEach((item) => {
+        const clone = template.content.cloneNode(true);
+        clone.querySelector('.text-white .lead').textContent = item.titulo
         clone.querySelector('.badge').textContent = item.cantidad
+        clone.querySelector('div .lead span').textContent = item.precio * item.cantidad;
         
-         fragment.appendChild(clone);
+        clone.querySelector('.btn-danger').dataset.id = item.id
+        clone.querySelector('.btn-success').dataset.id = item.id
+        fragment.appendChild(clone);
 
-      })
+    })
 
-      carrito.appendChild(fragment);
+    carrito.appendChild(fragment);
 
-  }
+    pintarFooter();
 
-  botones.forEach((btn) => btn.addEventListener("click", agregarAlCarrito ));
+};
+
+const pintarFooter = () => {
+    console.log("pintar footer");
+    footer.textContent = ""
+
+    const total = carritoObjeto.reduce(
+        (acc, current) => acc + current.cantidad * current.precio , 0 
+    )
+
+    const clone = templateFooter.content.cloneNode(true);
+    clone.querySelector('span').textContent = total
+
+    footer.appendChild(clone);
+
+}
+  
+const btnAumentar = (e) => {
+    console.log("me diste click", e.target.dataset.id);
+    carritoObjeto = carritoObjeto.map((item) => {
+        if (item.id === e.target.dataset.id) {
+            item.cantidad++;
+        }
+        return item
+
+    });
+    pintarCarrito();
+};
+
+const btnDisminuir = (e) => {
+    console.log("me diste click", e.target.dataset.id);
+
+    carritoObjeto = carritoObjeto.filter(item => {
+        if (item.id === e.target.dataset.id) {
+            if (item.cantidad > 0) {
+                item.cantidad--
+                if (item.cantidad === 0) return
+                return item
+            }
+        } else {
+            return item
+        }
+    })
+
+    pintarCarrito();
+}
+
+
+//   botones.forEach((btn) => btn.addEventListener("click", agregarAlCarrito )); forma antigua
 
 // const frutas = ["🍌", "🍏", "🍓"];
 
